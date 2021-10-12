29b963de135ff214a6fdb4c1068b7ec1ace8c80a
Anton Mehlqvist <antonmehlqvist@outlook.com>
2021-10-12 21:27:13 +0200

    bumped version and fixed missing file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 import alfy from 'alfy';
 import os from 'os';
-const homedir = os.homedir();
+import { readdirSync } from 'fs';
 
-import { getDirectories, trimProjectName } from './helpers.js';
+const homedir = os.homedir();
 
 const filterArg = process.argv[ 2 ];
 
@@ -25,3 +25,23 @@ alfy.output( directories.map( dirName => (
 		}
 	)
 ) );
+
+function getDirectories( source ) {
+	return readdirSync( source, { withFileTypes: true } )
+		.filter( dirent => dirent.isDirectory() )
+		.map( dirent => dirent.name );
+}
+
+function trimProjectName( dirName ) {
+    const isAcProject = dirName.includes( 'dev.synot.io' );
+
+    if ( isAcProject ) {
+        const dirNameParts = dirName.split( '.' );
+        dirNameParts.length = dirNameParts.length - 3;
+        const newDirName = dirNameParts.join( '.' );
+
+        return newDirName;
+    }
+
+    return dirName;
+};
